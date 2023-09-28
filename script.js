57b373e8782238d0319edc58e9ec1173cea37ac8
Ashish M <ashish_mahto@homedepot.com>
2023-09-28 15:45:35 -0400

    - world MAIN - analyze index source to find correct js module - refactor to move more traversal code out of `override` and into `main` - runAt - updated README.md

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 
-function overrideAggregated(TrnModule) {
-  window.TrnModule = TrnModule;
-  // the current season
+/**
+ * Replace the `fetchAggregated` function in the module with a version that fixes the missing parameter bug.
+ * @param acquaintanceMethods An object that contains helpers that call teammate related Tracker.gg APIs.
+ */
+function overrideAggregated(acquaintanceMethods) {
   const { defaultSeason } = window.__INITIAL_STATE__.stats.standardProfiles[0].metadata;
-
-  // find the correct export
-  const TrnAcquaintances = Object.entries(TrnModule).find(([, { name }]) => name === "TrnAcquaintances")[0];
-
-  // save the old fetch function
-  const { fetchAggregated } = TrnModule[TrnAcquaintances].methods;
-
-  TrnModule[TrnAcquaintances].methods.fetchAggregated = async function () {
-
-    // add the missing Act ID
+  const { fetchAggregated } = acquaintanceMethods;
+  acquaintanceMethods.fetchAggregated = async function () {
     this.filter.seasonId = this.filter.seasonId ?? defaultSeason;
-
-    // call the old fetch function again
     return fetchAggregated.apply(this, arguments);
   };
-};
+}
 
+/**
+ * Find the module responsible for valorant routes in index.js, and send it to `overrideAggregated`
+ * @require Assumes the modules' `link` positions are relatively static over Tracker site updates.
+ */
 function main() {
-  const TrnModules = [...document.querySelectorAll('link[rel="modulepreload"]')].map((x) => x.href);
-  import(TrnModules[7]).then((TrnModule) => overrideAggregated(TrnModule));
-};
+  const indexJs = document.querySelector('script[type="module"]').src;
+  fetch(indexJs)
+    .then((r) => r.text())
+    .then((source) => {
+      const i = source.indexOf("/valorant/config/routes.js");
+      const a = source.indexOf("./", i);
+      const b = source.indexOf(".js", a);
+      const jsFile = indexJs.replace(/index[\w.]*js/, source.slice(a + 2, b + 3));
+      import(jsFile).then((RouterModule) => RouterModule.default()
+        .flatMap((x) => x.children ?? [])
+        .find(({path}) => path === "matches")
+        .component()
+        .then((TrnModule) => overrideAggregated(TrnModule.default.components.Acquaintances.methods))
+      )
+    });
+}
 
-const script = document.createElement('script');
-script.type = 'module';
-script.textContent = `
-import {a, A} from './51479c59.js';
-console.log({a, A});
-`;
-document.head.appendChild(script);
+// run it at the next available event loop, so that DOM elements are available
+setTimeout(main, 0);
