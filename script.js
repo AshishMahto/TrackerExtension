9be77f5878dbd1f0030434bed50af40024537738
Ashish M <ashish_mahto@homedepot.com>
2023-09-30 10:17:07 -0400

    - simpler fix - new icon

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
 
-/**
- * Replace the `fetchAggregated` function in the module with a version that fixes the missing parameter bug.
- * @param acquaintanceMethods An object that contains helpers that call teammate related Tracker.gg APIs.
- */
-function overrideAggregated(acquaintanceMethods) {
-  const { defaultSeason } = window.__INITIAL_STATE__.stats.standardProfiles[0].metadata;
-  const { fetchAggregated } = acquaintanceMethods;
-  acquaintanceMethods.fetchAggregated = async function () {
-    this.filter.seasonId = this.filter.seasonId ?? defaultSeason;
-    return fetchAggregated.apply(this, arguments);
-  };
-}
+/** Essentially a Tracker.gg bugfix */
+fixSeasonFilter = (TrnModule) => TrnModule.default.computed.currentSeasonFilter = function () { return this.filter };
 
 /**
- * Find the module responsible for valorant routes in index.js, and send it to `overrideAggregated`
+ * Find the module responsible for valorant routes, and send it to `fixSeasonFilter`
  * @require Assumes the modules' `link` positions are relatively static over Tracker site updates.
  */
 function main() {
-  const indexJs = document.querySelector('script[type="module"]').src;
-  fetch(indexJs)
-    .then((r) => r.text())
-    .then((source) => {
-      const i = source.indexOf("/valorant/config/routes.js");
-      const a = source.indexOf("./", i);
-      const b = source.indexOf(".js", a);
-      const jsFile = indexJs.replace(/index[\w.]*js/, source.slice(a + 2, b + 3));
-      import(jsFile).then((RouterModule) => RouterModule.default()
-        .flatMap((x) => x.children ?? [])
-        .find(({path}) => path === "matches")
-        .component()
-        .then((TrnModule) => overrideAggregated(TrnModule.default.components.Acquaintances.methods))
-      )
-    });
+  const links = document.head.querySelectorAll('link[rel="modulepreload"]');
+  if (!links) return;
+  import(links[6].href).then(fixSeasonFilter);
+  document.head.removeEventListener('DOMNodeInserted', main);
 }
 
 // run it at the next available event loop, so that DOM elements are available
-setTimeout(main, 0);
+setTimeout(() => document.head.addEventListener('DOMNodeInserted', main), 0)
